c84d661de078b5683a84916d2e22160fc1ff2d7c
inRiza <icon.firdaus@gmail.com>
2026-04-04 09:50:00 +0700

    feat(profle): add new profile page with home redirection

--- app/src/main/java/com/example/nimons360/MainActivity.kt
+++ app/src/main/java/com/example/nimons360/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.nimons360
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import com.example.nimons360.R
+import androidx.navigation.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,5 +12,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_profile -> {
+                val navController = findNavController(R.id.nav_host_fragment)
+                if (navController.currentDestination?.id != R.id.profileFragment) {
+                    navController.navigate(R.id.profileFragment)
+                }
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/com/example/nimons360/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/nimons360/ui/home/HomeFragment.kt
@@ -1,8 +1,25 @@
 package com.example.nimons360.ui.home
 
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.nimons360.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeFragment : Fragment(R.layout.fragment_home)
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnProfile = view.findViewById<TextView>(R.id.btnProfileHome)
+
+        btnProfile.text = "L" // mock
+
+        btnProfile.setOnClickListener {
+            findNavController().navigate(R.id.action_home_to_profile)
+        }
+    }
+}
