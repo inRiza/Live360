1d98ccb46206b5b828d4c14ef44098d3791c697c
rlukassa <19623069@std.stei.itb.ac.id>
2026-04-13 23:34:10 +0700

    feat: apirunner and refactor login

---
+++ api-runner.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+SPEC="openapi.yaml"
+JAVA_ROOT="app/src/main/java"
+BASE_PACKAGE="com.example.nimons360.data.remote.dto"
+BASE_PATH="${JAVA_ROOT}/${BASE_PACKAGE//./\/}"
+
+GENERATOR_JAR="openapi-generator-cli.jar"
+GENERATOR_VERSION="7.2.0"
+
+if [ ! -f "$GENERATOR_JAR" ]; then
+    curl -L "https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/$GENERATOR_VERSION/openapi-generator-cli-$GENERATOR_VERSION.jar" -o "$GENERATOR_JAR"
+fi
+
+rm -rf "$BASE_PATH"
+mkdir -p "$BASE_PATH/request"
+mkdir -p "$BASE_PATH/response"
+mkdir -p "$BASE_PATH/common"
+
+rm -rf tmp_gen
+# Added --type-mappings=URI=String to prevent URI mismatch errors
+java -jar "$GENERATOR_JAR" generate \
+    -i "$SPEC" \
+    -g kotlin \
+    -o tmp_gen \
+    --type-mappings=URI=String \
+    --global-property models \
+    --additional-properties=modelPackage=$BASE_PACKAGE,enumPropertyNaming=UPPERCASE,serializationLibrary=gson
+
+GEN_FILES="tmp_gen/src/main/kotlin/${BASE_PACKAGE//./\/}"
+
+for file in "$GEN_FILES"/*.kt; do
+    [ -e "$file" ] || continue
+    filename=$(basename "$file")
+
+    if [[ "$filename" == *Request.kt ]]; then
+        TARGET_DIR="$BASE_PATH/request"
+        TARGET_PKG="$BASE_PACKAGE.request"
+    elif [[ "$filename" == *Response.kt ]]; then
+        TARGET_DIR="$BASE_PATH/response"
+        TARGET_PKG="$BASE_PACKAGE.response"
+    else
+        TARGET_DIR="$BASE_PATH/common"
+        TARGET_PKG="$BASE_PACKAGE.common"
+    fi
+
+    sed -i "s/package $BASE_PACKAGE/package $TARGET_PKG/g" "$file"
+
+    sed -i "/import $BASE_PACKAGE/d" "$file"
+
+    sed -i "/package $TARGET_PKG/a \\
+\\
+import $BASE_PACKAGE.request.*\\
+import $BASE_PACKAGE.response.*\\
+import $BASE_PACKAGE.common.*" "$file"
+
+    mv "$file" "$TARGET_DIR/"
+done
+
+rm -rf tmp_gen
--- app/src/main/java/com/example/nimons360/MainActivity.kt
+++ app/src/main/java/com/example/nimons360/MainActivity.kt
@@ -1,14 +1,31 @@
 package com.example.nimons360
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.nimons360.R
+import com.example.nimons360.data.local.preference.TokenPreference
+import com.example.nimons360.ui.auth.login.LoginActivity
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var tokenPreference: TokenPreference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Cek apakah user sudah login
+        if (tokenPreference.getToken().isNullOrEmpty()) {
+            // Jika belum login, arahkan ke LoginActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+            return
+        }
+
         setContentView(R.layout.activity_main)
     }
 }
