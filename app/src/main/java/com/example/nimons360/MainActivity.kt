f05cee973ed9ea926e9c4b98cd65bdd5d3406d2b
inRiza <icon.firdaus@gmail.com>
2026-04-18 00:10:01 +0700

    feat(nav): add navigation requirements on top_bar and bottom_bar

--- app/src/main/java/com/example/nimons360/MainActivity.kt
+++ app/src/main/java/com/example/nimons360/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.nimons360
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.nimons360.data.local.preference.TokenPreference
 import com.example.nimons360.ui.auth.login.LoginActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -27,5 +31,21 @@ class MainActivity : AppCompatActivity() {
         }
 
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav) // menggunakan non-binding karena langsung via layout
+        bottomNav.setupWithNavController(navController)
+
+        val title = findViewById<TextView>(R.id.tv_top_bar_title)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            title.text = destination.label?.toString() ?: "Nimons360"
+        }
+
+        val avatar = findViewById<TextView>(R.id.tv_top_bar_avatar)
+        avatar.setOnClickListener {
+            // TODO: navigate ke profileFragment
+            // navContoller.navigate(R.id.profileFragment)
+        }
     }
 }
