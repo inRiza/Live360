a1daacb71344ba989df93be9ed23d35deb8b9055
inRiza <icon.firdaus@gmail.com>
2026-04-18 14:52:33 +0700

    refactor(websocket): add gson annotations to and for improved json serialization

--- app/src/main/java/com/example/nimons360/data/remote/websocket/model/MemberPresencePayload.kt
+++ app/src/main/java/com/example/nimons360/data/remote/websocket/model/MemberPresencePayload.kt
@@ -1,14 +1,14 @@
 package com.example.nimons360.data.remote.websocket.model
-
+import com.google.gson.annotations.SerializedName
 data class MemberPresencePayload(
-    val userId: Int,
-    val email: String,
-    val fullName: String,
-    val latitude: Double,
-    val longitude: Double,
-    val rotation: Float,
-    val batteryLevel: Int,
-    val isCharging: Boolean,
-    val internetStatus: String,
-    val metadata: Map<String, Any> = emptyMap()
+    @SerializedName("userId") val userId: Int,
+    @SerializedName("email") val email: String,
+    @SerializedName("fullName") val fullName: String,
+    @SerializedName("latitude") val latitude: Double,
+    @SerializedName("longitude") val longitude: Double,
+    @SerializedName("rotation") val rotation: Float,
+    @SerializedName("batteryLevel") val batteryLevel: Int,
+    @SerializedName("isCharging") val isCharging: Boolean,
+    @SerializedName("internetStatus") val internetStatus: String,
+    @SerializedName("metadata") val metadata: Map<String, Any> = emptyMap()
 )
